396007957959eaf03cbe72886d290a21a6f3ac1a
Manohar Singh <Manohar.Singh@nathcorp.com>
2020-07-03 15:56:31 +0530

    first commit

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
-
+import os
 from aws_cdk import core
 
 from psc_ebs.psc_ebs_stack import PscEbsStack
 
 
 app = core.App()
-PscEbsStack(app, "psc-ebs")
+PscEbsStack(app, "psc-ebs", env=core.Environment(
+    account=os.environ.get("CDK_DEPLOY_ACCOUNT", os.environ["CDK_DEFAULT_ACCOUNT"]),
+    region=os.environ.get("CDK_DEPLOY_REGION", os.environ["CDK_DEFAULT_REGION"])))
 
 app.synth()
--- psc_ebs/psc_ebs_stack.py
+++ psc_ebs/psc_ebs_stack.py
@@ -1,4 +1,6 @@
-from aws_cdk import core
+from aws_cdk import (
+    core,
+    aws_ec2 as ec2)
 
 
 class PscEbsStack(core.Stack):
@@ -7,3 +9,22 @@ class PscEbsStack(core.Stack):
         super().__init__(scope, id, **kwargs)
 
         # The code that defines your stack goes here
+        # AMI 
+        
+        vpc = ec2.Vpc.from_lookup(self, "VPC",vpc_id = "vpc-0ef6465a4998b9e95"
+        ) 
+        amzn_linux = ec2.MachineImage.latest_amazon_linux(
+            generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+            edition=ec2.AmazonLinuxEdition.STANDARD,
+            virtualization=ec2.AmazonLinuxVirt.HVM,
+            storage=ec2.AmazonLinuxStorage.GENERAL_PURPOSE
+            )
+
+        # Instance
+        instance = ec2.Instance(self, "Instance",
+            instance_type=ec2.InstanceType("t3.nano"),
+            machine_image=amzn_linux,
+            vpc = vpc
+            )
+
+
